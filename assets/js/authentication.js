6efa8842ecd70b8012ab7f54a7287cbe7e25f53e
Arman <140567994+veltrane@users.noreply.github.com>
2026-05-30 18:47:44 +0500

    Added a register, and verify page

--- assets/js/authentication.js
+++ assets/js/authentication.js
@@ -1,6 +1,7 @@
 const passwordVisibility = document.querySelector('.vl__password button');
 const passwordInput = document.querySelector('.vl__password input');
 const [iconOff, iconOn] = passwordVisibility.querySelectorAll('img');
+const codeInput = document.querySelector('input[inputmode="numeric"]');
 
 passwordVisibility.addEventListener('click', () => {
   const isHidden = passwordInput.type === 'password';
@@ -11,4 +12,8 @@ passwordVisibility.addEventListener('click', () => {
 
 passwordVisibility.addEventListener('pointerdown', e => {
   if (e.pointerType === 'touch') e.preventDefault();
+});
+
+codeInput.addEventListener('input', () => {
+  codeInput.value = codeInput.value.replace(/\D/g, '');
 });
